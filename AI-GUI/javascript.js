e8d5318e8893bcb4de91a81f79100bc5bed62121
Sachin Kamath <157329357+sachin1kamath@users.noreply.github.com>
2026-05-23 18:45:41 -0400

    Finally fixed the AI GUI. It now properly recognizes the Ollama API and model and properly displys the response in a div. It also streams the response, meaning that each word is displayed one at a time, like in a real chat.

--- AI-GUI/javascript.js
+++ AI-GUI/javascript.js
@@ -1,20 +1,22 @@
 import Ollama from 'ollama';
 
-async function getInput() {
+async function displayResponse() {
+    const responseDiv = document.getElementById('response');
+    responseDiv.textContent = '';
+
     let input = String(document.getElementById('input').value);
 
-    const response = await ollama.chat({
+    const response = await Ollama.chat({
         model: 'qwen3.5:9b',
         messages: [{ role: 'user', content: input }],
+        stream: true,
     });
-    console.log(response.message.content);
-    return input;
-};
-
-function displayResponse(response) {
-    const responseDiv = document.getElementById('response');
-    responseDiv.innerHTML = `<p>${String(response)}</p>`;
+    for await (const part of response) {
+        console.log(part.message.content)
+        responseDiv.textContent += part.message.content;
+    }
+    const inputBox = document.getElementById('input');
+    input.textContent = 'Input'
 };
 
 window.displayResponse = displayResponse;
-window.getInput = getInput; 
